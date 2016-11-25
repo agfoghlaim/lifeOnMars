9d52e6a3ebd8ba874e7f95a3cb0df24e58be204b
Agfoghlaim <marieohara.gti@gmail.com>
2016-11-25 12:38:52 +0000

    tidied controller and settings template

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,30 +1,17 @@
 //SETTINGS CONTROLLER
 
 app.controller('groundControl', function($scope, service1){
-	$scope.defaultValue = 1;
-	$scope.numberDays = function(numDays){
-		switch(numDays) {
-		 	case 7:
-		 		service1.getSet(7);
-		 	break;
-		 	case 14:
-		 		service1.getSet(14);
-		 	break;
-		 	default:
-		 		service1.getSet(1);
-		}
-	}
-// $scope.amount = {days:1};
-// $scope.choices = [1,7,14];
-// console.log($scope.amount.days);
 
-// $scope.changeNoDays = function(choice){
-// console.log(choice);
-// service1.getSet(choice);
-// }
+$scope.amount = {days:1};
+$scope.choices = [1,7,14];
+$scope.changeNoDays = function(choice){
+service1.getSet(choice);
+}
 
 
 });
+
+
 
 app.controller('majorTom', function($scope, service1, service2){
 	var noOfDays = service1.getSet();
