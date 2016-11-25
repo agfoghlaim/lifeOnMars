a5ae8e9339faeb90691572fbd5dac5d6f5e70e02
Agfoghlaim <marieohara.gti@gmail.com>
2016-11-25 12:29:27 +0000

    fixed service1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,33 +1,40 @@
 //SETTINGS CONTROLLER
 
 app.controller('groundControl', function($scope, service1){
-
-
+	$scope.defaultValue = 1;
 	$scope.numberDays = function(numDays){
 		switch(numDays) {
 		 	case 7:
-		 		service1.setDays(7);
+		 		service1.getSet(7);
 		 	break;
 		 	case 14:
-		 		service1.setDays(14);
+		 		service1.getSet(14);
 		 	break;
 		 	default:
-		 		service1.setDays(1);
-		 }
+		 		service1.getSet(1);
+		}
 	}
+// $scope.amount = {days:1};
+// $scope.choices = [1,7,14];
+// console.log($scope.amount.days);
+
+// $scope.changeNoDays = function(choice){
+// console.log(choice);
+// service1.getSet(choice);
+// }
+
+
 });
 
 app.controller('majorTom', function($scope, service1, service2){
-
-	var noOfDays = service1.getDays();
+	var noOfDays = service1.getSet();
 	service2.getPhoto(responseGood, responseBad, noOfDays);
 	$scope.nasaPhotosArray=[];
 
+
 	function responseGood(response){
-
 		$scope.nasaPhotosArray.push(response);
-
-		$scope.nasaPhotosArray = $scope.nasaPhotosArray.sort(function(a,b){
+		$scope.nasaPhotosArray.sort(function(a,b){
 			if(a.date>b.date){
 				return -1;
 			}
@@ -36,15 +43,13 @@ app.controller('majorTom', function($scope, service1, service2){
 			}else{
 				return 0 ;
 			}
-	
 		});
-
 	};
 
 	function responseBad(status, text){
-		if(status != 500){  
-		alert("ERROR: " + status +" " + text);
-	}
+		if(status != 500){  //no potd returns 500, ignore this error
+		alert("ERROR: " + status + " " + text);
+		}
 	};
 
 		
